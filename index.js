8302ff882d22ee56138780821ddd46ef97a573dc
ramonac <ramonacirlan@yahoo.com>
2024-02-05 17:01:15 +0000

    function to write Readme file

--- index.js
+++ index.js
@@ -9,58 +9,63 @@ const questions = [
     type: 'input',
     name: 'The title of my project',
     message: 'Provide the name of your Project:',
-}
+},
 {
     type: 'input', 
     name: 'Description',
     message: 'Enter project description:',
-}
+},
 {
     type: 'input',
     name: 'Table of Contents',
     message: 'Provide the table of contents:',
-}
+},
 {
     type: 'input',
     name: 'Installation',
     message: 'Provide the installation requirements:',
-}
+},
 {
     type: 'input',
     name: 'Usage',
-    message: 'Enter how to use the app:' ,
-}
+    message: 'Enter how to use the app:',
+},
 {
     type: 'input',
     name: 'License',
     message: 'Choose a license',
-    choices: [
+    choices:[
         'MIT Licence', 
         'Apache License 2.0', 
         'GNU General Public License', 
         'Berkeley Software Distribution License'
     ]
-}
+},
 {
     type: 'input',
     name: 'Contributing',
     message: 'Provide list of contributors:',
-}
+},
 {
     type: 'input',
     name: 'Tests',
     message: 'Are there any tests included?',
-}
+},
 {
     type: 'input',
     name: 'Questions',
     message:'What is your GitHub username?',
-}
-    
+}, 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('Success!, the README.md file has been created!');
+    });
 }
 
 // function to initialize program
